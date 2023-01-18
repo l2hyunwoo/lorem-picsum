ccdccad82a095110dc69bddd19794aa27c867cb0
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-01-19 00:51:26 +0900

    :recycle: Use util code on MainActivity

--- app/src/main/java/com/github/hyunwoo/picsum/MainActivity.kt
+++ app/src/main/java/com/github/hyunwoo/picsum/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.github.hyunwoo.picsum
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import com.github.hyunwoo.picsum.common.activity.BindingActivity
 import com.github.hyunwoo.picsum.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
     }
 }
