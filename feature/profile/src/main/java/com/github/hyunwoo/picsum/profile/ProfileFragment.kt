1b18d0db3b0e03c64760a51e9abb7bd2843001ac
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-01-19 10:36:59 +0900

    :art: Add ui files in profile module

---
+++ feature/profile/src/main/java/com/github/hyunwoo/picsum/profile/ProfileFragment.kt
@@ -0,0 +1,12 @@
+package com.github.hyunwoo.picsum.profile
+
+import android.os.Bundle
+import android.view.View
+import com.github.hyunwoo.picsum.common.fragment.BindingFragment
+import com.github.hyunwoo.picsum.profile.databinding.FragmentProfileBinding
+
+class ProfileFragment : BindingFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
