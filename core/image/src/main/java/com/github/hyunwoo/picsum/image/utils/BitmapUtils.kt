157257d095fea3caa6f4cc73795c55bea3801da2
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-01-21 03:15:12 +0900

    :sparkles: Add decode utils for Bitmap

---
+++ core/image/src/main/java/com/github/hyunwoo/picsum/image/utils/BitmapUtils.kt
@@ -0,0 +1,38 @@
+package com.github.hyunwoo.picsum.image.utils
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+
+internal fun ByteArray.decodeSampledBitmap(
+    requiredHeight: Int,
+    requiredWidth: Int
+): Bitmap {
+    require(requiredWidth >= 0 && requiredHeight >= 0) { "Width and height must be greater than 0" }
+
+    return BitmapFactory.Options().run {
+        inJustDecodeBounds = true
+        BitmapFactory.decodeByteArray(this@decodeSampledBitmap, 0, size, this)
+        inSampleSize = calculateInSampleSize(requiredWidth, requiredHeight)
+        inJustDecodeBounds = false
+        BitmapFactory.decodeByteArray(this@decodeSampledBitmap, 0, size, this)
+    }
+}
+
+private fun BitmapFactory.Options.calculateInSampleSize(
+    requestHeight: Int,
+    requestWidth: Int
+): Int {
+    val (height, width) = this.run { outHeight to outWidth }
+    var inSampleSize = 1
+
+    if (height > requestHeight || width > requestWidth) {
+        val halfHeight = height / 2
+        val halfWidth = width / 2
+
+        while ((halfHeight / inSampleSize) >= requestHeight && (halfWidth / inSampleSize) >= requestWidth) {
+            inSampleSize *= 2
+        }
+    }
+
+    return inSampleSize
+}
