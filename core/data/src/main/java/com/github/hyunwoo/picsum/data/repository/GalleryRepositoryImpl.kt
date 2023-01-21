795fe52336b2ee48a574a72b1dbaac37bbf634e2
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-01-21 22:31:24 +0900

    :art: Make Domain Logic

--- core/data/src/main/java/com/github/hyunwoo/picsum/data/repository/GalleryRepositoryImpl.kt
+++ core/data/src/main/java/com/github/hyunwoo/picsum/data/repository/GalleryRepositoryImpl.kt
@@ -1,15 +1,24 @@
 package com.github.hyunwoo.picsum.data.repository
 
-import com.github.hyunwoo.picsum.data.api.GalleryApi
+import com.github.hyunwoo.picsum.data.local.PhotoDao
 import com.github.hyunwoo.picsum.data.mapper.asDomainModel
+import com.github.hyunwoo.picsum.data.mapper.toEntity
 import com.github.hyunwoo.picsum.domain.entity.Photo
 import com.github.hyunwoo.picsum.domain.repository.GalleryRepository
 import javax.inject.Inject
 
 class GalleryRepositoryImpl @Inject constructor(
-    private val network: GalleryApi
+    private val dao: PhotoDao
 ) : GalleryRepository {
-    override suspend fun getPhotos(page: Int): List<Photo> {
-        return network.getPhotos(page).map { it.asDomainModel() }
+    override suspend fun getLikedPhotos(): List<Photo> {
+        return dao.getAll().map { it.asDomainModel() }
+    }
+
+    override suspend fun likePhoto(photo: Photo) {
+        dao.insert(photo.toEntity())
+    }
+
+    override suspend fun unlikePhoto(photo: Photo) {
+        dao.delete(photo.toEntity())
     }
 }
